659d888ecd062951b8357bba44313939954a80a9
Lea <lofrimpong18@gmail.com>
2023-02-23 08:27:50 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _isuper - Checks if a letter is super
+ * _isupper - Checks if a letter is super
  * @c: The number to be checked
  * Return: 1 for super letter and 0 for anything else
  */
 
 int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
