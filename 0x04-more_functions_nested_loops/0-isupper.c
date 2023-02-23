b2c7efd4240077596fb6e4a621436750b9d866f2
Lea <lofrimpong18@gmail.com>
2023-02-23 02:09:28 -0800

    supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isuper - Checks if a letter is super
+ * @c: The number to be checked
+ * Return: 1 for super letter and 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
