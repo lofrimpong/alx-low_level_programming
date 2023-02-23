d37de2a21a0ec637b7e22becf311f7fbbf80f945
Lea <lofrimpong18@gmail.com>
2023-02-23 03:29:36 -0800

    main c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@
  * main - Test function for positive or negative
  * Return: 0
  */
-
 int main(void)
 
 {
@@ -12,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
-return (0);
+	
+	return (0);
+}
