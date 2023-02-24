f1dd908eb46b88dd03220e10fafc672d57363fbd
Lea <lofrimpong18@gmail.com>
2023-02-24 10:50:10 -0800

    prints

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+print_number(98);
+putchar('\n');
+print_number(402);
+putchar('\n');
+print_number(1024);
+putchar('\n');
+print_number(0);
+putchar('\n');
+print_number(-98);
+putchar('\n');
+return (0);
+}
